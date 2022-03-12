621993be893a6ee02b4aa2da1a94b688bc9a5dd2
labeebk1 <labeebk1@gmail.com>
2022-03-12 07:24:41 -0500

    fixed download option

--- bot.py
+++ bot.py
@@ -116,10 +116,10 @@ async def download(ctx,song):
     await ctx.send(embed=embed)
     try:
         filename = await YTDLSource.from_url(song, loop=bot.loop)
+        download_queue.remove(song)
     except Exception:
         download_queue.remove(song)
     
-    download_queue.remove(song)
 
 @bot.command(name='remove', aliases=["r"], help='Remove from song')
 async def remove(ctx, pos_to_remove):
